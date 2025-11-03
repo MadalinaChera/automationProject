5f8be91c082a6cc9fe35c23bf634af932a924ac7
MadalinaChera <mail.madalina.chera@gmail.com>
2025-11-03 20:28:44 +0200

    Am terminat de refractorizat

--- src/test/java/tests/WebTableTest1.java
+++ src/test/java/tests/WebTableTest1.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.ElementsPage;
+import pages.HomePage;
+import pages.WebTablePage;
 
 import java.util.List;
 
@@ -19,118 +22,36 @@ public class WebTableTest1 {
 
     @Test
 
-    public void metodaTest (){
+    public void metodaTest() {
 
-        //Deschidem un browser
-
-        driver=new ChromeDriver();
-        elementsMethod = new ElementsMethod(driver);
-
-        //Accesam un URL
-
+        driver = new ChromeDriver();
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
+        elementsMethod = new ElementsMethod(driver);
 
-        WebElement elementsMeniu = driver.findElement(By.xpath("//h5[text()='Elements']"));
+        HomePage homePage = new HomePage(driver);
+        homePage.clickElementsMenu();
 
-        JavascriptExecutor js = (JavascriptExecutor)driver;
-        elementsMethod.javaScriptElement(elementsMeniu);
+        ElementsPage elementsPage = new ElementsPage(driver);
+        elementsPage.clickWebTable();
 
+        WebTablePage webTablePage = new WebTablePage(driver);
+        webTablePage.createProcess();
 
-        WebElement webTable = driver.findElement(By.xpath("//span[text()='Web Tables']"));
-        webTable.click();
+        //Functionalitate de edit
 
-        List<WebElement> continutTabel= driver.findElements(By.xpath("//div[@class='rt-tbody']/div/div[@class='rt-tr -odd' or @class='rt-tr -even']"));
-//        System.out.println("Diminesiunea tabelului este "+continutTabel.size());
-        Assert.assertEquals(continutTabel.size(),3,"Marimea tabelului nu este 3 ");
+        webTablePage.editProcess();
+        webTablePage.deleteProcess();
 
+        //Daca vreti sa identificati un elemeent dupa text puteti face asta doar cu 'XPath'.
+        //CSS Selector nu permite acest lucru
+        //XPath are o structura specifica: tag, atribut, valoare
 
+//        driver.close();
 
-// Definim un element
-        WebElement addElement = driver.findElement(By.id("addNewRecordButton"));
-        addElement.click();
-
-// First Name
-        WebElement firstNameElement = driver.findElement(By.id("firstName"));
-        String firstName = "Madalina";
-        elementsMethod.fillElement(firstNameElement, firstName);
-
-// Last Name
-        WebElement lastNameElement = driver.findElement(By.id("lastName"));
-        String lastName = "Chera";
-        elementsMethod.fillElement(lastNameElement, lastName);
-
-// Email
-        WebElement userEmailElement = driver.findElement(By.id("userEmail"));
-        String userEmail = "test@gmail.com";
-        elementsMethod.fillElement(userEmailElement, userEmail);
-
-// Age
-        WebElement ageElement = driver.findElement(By.id("age"));
-        String age = "30";
-        elementsMethod.fillElement(ageElement, age);
-
-// Salary
-        WebElement salaryElement = driver.findElement(By.id("salary"));
-        String salary = "5000";
-        elementsMethod.fillElement(salaryElement, salary);
-
-// Department
-        WebElement departmentElement = driver.findElement(By.id("department"));
-        String department = "Quality";
-        elementsMethod.fillElement(departmentElement, department);
-
-        WebElement submitButton = driver.findElement(By.id("submit"));
-        submitButton.click();
-        //Validam noua dimensiune a tabelului
-        List<WebElement> continutTabelNou= driver.findElements(By.xpath("//div[@class='rt-tbody']/div/div[@class='rt-tr -odd' or @class='rt-tr -even']"));
-        Assert.assertEquals(continutTabelNou.size(),4,"Marimea tabelului nu este 4 ");
-        //Validam valorile pe care le-am introdus
-        String continutRand = continutTabelNou.get(3).getText();
-        Assert.assertTrue(continutRand.contains(firstName), "Randul nu contine first name ");
-        Assert.assertTrue(continutRand.contains(lastName), "Randul nu contine last name");
-        Assert.assertTrue(continutRand.contains(userEmail), "Randul nu contine user email");
-        Assert.assertTrue(continutRand.contains(age), "Randul nu contine age");
-        Assert.assertTrue(continutRand.contains(salary), "Randul nu contine salary");
-        Assert.assertTrue(continutRand.contains(department), "Randul nu contine departament");
-
-
-        // Edit Functionality
-        WebElement editElement = driver.findElement(By.id("edit-record-4"));
-        editElement.click();
-
-        WebElement editfirstNameElement = driver.findElement(By.id("firstName"));
-        editfirstNameElement.clear();
-        String nume2 = "Richard";
-        editfirstNameElement.sendKeys(nume2);
-
-        WebElement editlastNameElement = driver.findElement(By.id("lastName"));
-        editlastNameElement.clear();
-        String nume3 = "Lavi";
-        editlastNameElement.sendKeys(nume3);
-
-        WebElement editEmailElement = driver.findElement(By.id("userEmail"));
-        editEmailElement.clear();
-        String email2 = "Lavi@gmail.com";
-        editEmailElement.sendKeys(email2);
-
-        WebElement editAgeElement = driver.findElement(By.id("age"));
-        editAgeElement.clear();
-        String age2 = "25";
-        editAgeElement.sendKeys(age2);
-
-        WebElement submitbutton2Button = driver.findElement(By.id("submit"));
-        submitbutton2Button.click();
-
-        WebElement deleteElement = driver.findElement(By.id("delete-record-4"));
-        deleteElement.click();
-
-        List<WebElement> continutTabelSters= driver.findElements(By.xpath("//div[@class='rt-tbody']/div/div[@class='rt-tr -odd' or @class='rt-tr -even']"));
-        Assert.assertEquals(continutTabelSters.size(),3, "Noul tabel nu are 3 randuri");
-
-        //driver.close();
     }
 }
+
 
 // Daca vrem sa identificam un element dupa text putem face asta doar cu XPath.
 // CSS Selector nu permite acest lucru.
